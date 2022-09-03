30313012a7a61a458bd5af92fe1d6bef2e5f35cd
Nirob Hasan <nirob427417@gmail.com>
2022-09-03 11:54:46 +0600

    category

--- js/index.js
+++ js/index.js
@@ -14,22 +14,27 @@ const setAllMenu = async () => {
 
     const menu = document.getElementById("all-menu");
 
-    const uniqueArray = [];
-
     for (const news of category) {
-        console.log(news);
-        // console.log(uniqueArray.indexOf(news.news_category[0]))
-
-        // uniqueArray.push(data);
+        //  console.log(news.category_name);
 
         const newsDiv = document.createElement('div');
         newsDiv.classList.add('col');
-        newsDiv.innerHTML = `<a>${data.news_category[0].category_name}</a>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>`;
+
+        newsDiv.innerHTML = `<a>${news.category_name}</a>
+        `;
+
         menu.appendChild(newsDiv);
 
     }
-
-    //  console.log(uniqueArray)
 }
+const displayNews = async () => {
+
+
+    const data = await loadNews();
+    const category = data.news_category;
+    console.log(category);
+}
+
+
 setAllMenu();
+displayNews();
