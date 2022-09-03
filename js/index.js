a382efe93e5b4e6c6b84b4380e8b39d15c3ccf87
Nirob Hasan <nirob427417@gmail.com>
2022-09-03 12:41:36 +0600

    allNews

--- js/index.js
+++ js/index.js
@@ -27,14 +27,51 @@ const setAllMenu = async () => {
 
     }
 }
-const displayNews = async () => {
+// const displayNews = async () => {
 
 
-    const data = await loadNews();
-    const category = data.news_category;
-    console.log(category);
+//     const data = await loadNews();
+//     const category = data.news_category;
+//     console.log(category);
+// }
+
+const loadNewsDetail = () => {
+
+    const url = `https://openapi.programming-hero.com/api/news/category/01`
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayNewsDetail(data.data));
 }
 
+const displayNewsDetail = newses => {
+    //  console.log(newses);
+    const newsDetail = document.getElementById('news-detail');
+    for (const news of newses) {
+        console.log(news);
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <div class="card mb-3" style="max-width: 100%;">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="${news.image_url}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">${news.title}</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+        </div>
+    </div>
+        `
+        newsDetail.appendChild(div);
+    }
 
+}
+
+loadNewsDetail();
 setAllMenu();
-displayNews();
+displayNewsDetail();
